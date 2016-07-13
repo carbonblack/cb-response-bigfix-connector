226bb7c35404a25df2dbb340ccf7fb860726d427
Doran Smestad <dsmestad@carbonblack.com>
2016-07-13 10:50:50 -0400

    Few more comments on the punisher code, todo's for later

---
+++ punisher/python/main.py
@@ -0,0 +1,19 @@
+
+class Punisher(object):
+    """
+    This class is the main class for the Punisher code. This will
+    init all our classes and do the main work.
+    """
+
+    # TODO need to have function to register as a windows service
+    # TODO also need to have a function to de-register a service.
+
+    def __init__(self):
+
+        pass
+        # TODO load in config with a configparser
+        # TODO setup a loop with some time delay, pull banned files at interval
+        # TODO take results from cb and create fixlets with them from there.
+        # TODO have some sort of logging facility to provide debug info
+
+
