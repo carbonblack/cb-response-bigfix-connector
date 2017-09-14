2d3e245f80378a8b1d0200f59accd1eb9d4385a7
Jason Garman <jgarman@carbonblack.com>
2017-09-14 15:09:04 -0400

    dumb errors...

--- test/vulnerable_binary/main.go
+++ test/vulnerable_binary/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	if len(os.Args) > 1 {
 		// run cmd.exe with ping
-		out, _ := exec.Command("cmd.exe /c ping 127.0.0.1").Output()
+		out, _ := exec.Command("ping", "127.0.0.1").Output()
 		fmt.Printf("%s", out)
 	} else {
 		fmt.Println("Hello world")
